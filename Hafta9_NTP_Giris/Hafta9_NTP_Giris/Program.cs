43e7b5cd5ed71622b2394e58b0a5503f24f25ebe
ayhanakkaya <ayhanakkayameb@gmail.com>
2023-11-06 15:08:28 +0300

    hafta9 işlendi

---
+++ Hafta9_NTP_Giris/Hafta9_NTP_Giris/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Hafta9_NTP_Giris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dikdörtgen Problemi");
+            Dikdortgen d = new Dikdortgen(13,14);
+            Console.WriteLine("Alan=" + d.alanHesapla());
+            Console.WriteLine("Çevre=" + d.cevreHesapla());
+            
+        }
+    }
+
+
+    class Dikdortgen
+    {
+        public int a, b;
+        public Dikdortgen(int a,int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public int alanHesapla()
+        {
+            return a * b;
+        }
+
+        public int cevreHesapla()
+        {
+            return 2 * (a + b);
+        }
+
+    }
+}
+
+
+
---
+++ Hafta9_NTP_Ogrenci/Hafta9_NTP_Ogrenci/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Hafta9_NTP_Ogrenci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci ayhan = new Ogrenci(11111,"Ayhan Akkaya",125,60);
+            Console.WriteLine("Öğrencinin TC :" + ayhan.tcNo);
+            Console.WriteLine("Öğrencinin Adı Soyadı :" + ayhan.adSoyad);
+            Console.WriteLine("Öğrencinin Okul No :" + ayhan.okulNo);
+            Console.WriteLine("Öğrencinin Ders Notu :" + ayhan.dersNot);
+            ayhan.devamDurumu(3);
+            ayhan.disiplin("Telefon");
+
+            Console.WriteLine("********************");
+            Ogrenci ismail = new Ogrenci(22222, "İsmail Arpacık", 1528, 40);
+            Console.WriteLine("Öğrencinin TC :" + ismail.tcNo);
+            Console.WriteLine("Öğrencinin Adı Soyadı :" + ismail.adSoyad);
+            Console.WriteLine("Öğrencinin Okul No :" + ismail.okulNo);
+            Console.WriteLine("Öğrencinin Ders Notu :" + ismail.dersNot);
+            ismail.devamDurumu(8);
+            ismail.disiplin("Okuldan Kaçma");
+
+            Console.WriteLine("********************");
+            Ogrenci yasemin = new Ogrenci(3333, "Yasemin Matar", 1859, 80);
+            Console.WriteLine("Öğrencinin TC :" + yasemin.tcNo);
+            Console.WriteLine("Öğrencinin Adı Soyadı :" + yasemin.adSoyad);
+            Console.WriteLine("Öğrencinin Okul No :" + yasemin.okulNo);
+            Console.WriteLine("Öğrencinin Ders Notu :" + yasemin.dersNot);
+            yasemin.devamDurumu(1);
+            yasemin.disiplin("Yok");
+
+        }
+    }
+
+    class Ogrenci
+    {
+        public double tcNo;
+        public String adSoyad;
+        public int okulNo;
+        public byte dersNot;
+
+        public Ogrenci(double tcNo,String adSoyad,int okulNo,byte dersNot)
+        {
+            this.tcNo = tcNo;
+            this.adSoyad = adSoyad;
+            this.okulNo = okulNo;
+            this.dersNot = dersNot;
+
+        }
+
+        public void devamDurumu(int i)
+        {
+            Console.WriteLine("Öğreencinin Devamsızlık günü:" + i);
+        }
+
+        public void disiplin(String a)
+        {
+            Console.WriteLine("Öğrencinin Disiplin suçu:" + a);
+        }
+    }
+}
