5ec5a966a5e122b671027647e1d0beefef1fe8e4
ayhanakkaya <ayhanakkayameb@gmail.com>
2023-11-20 09:26:52 +0300

    hafta11 ilk 2 uygulama eklendi

---
+++ Hafta11_NTP_Tekrar/Hafta11_NTP_Tekrar/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hafta11_NTP_Tekrar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**************Hafta11**************");
+           
+            Console.WriteLine("**************************");
+            Dersler mat = new Dersler();
+            mat.dersAdi = "Matematik";
+            mat.dersSaati = 4;
+            mat.dersKredi = 6;
+            mat.dersNotu = 60;
+            mat.islenenKonu = "Kümeler";
+            mat.dersSecme();
+            mat.dersGecme();
+            Console.WriteLine("**************************");
+            Dersler ntp = new Dersler();
+            ntp.dersAdi = "Nesne Tabanlı Programlama";
+            ntp.dersSaati = 10;
+            ntp.dersKredi = 10;
+            ntp.dersNotu = 40;
+            ntp.islenenKonu = "Sınıflar";
+            ntp.dersSecme();
+            ntp.dersGecme();
+
+            Console.ReadKey();
+        }
+    }
+}
+
+class Dersler
+{
+    //Derslere ait özellikler
+    public String dersAdi;
+    public byte dersSaati;
+    public byte dersKredi;
+    public String islenenKonu;
+    public int dersNotu;
+
+    public void dersSecme()
+    {
+        Console.WriteLine(dersAdi + " dersi seçildi");
+        Console.WriteLine(dersAdi + " dersi Saati:"+dersSaati);
+        Console.WriteLine(dersAdi + " dersi kredisi:"+dersKredi);  
+    }
+
+    public void dersGecme()
+    {
+        if (dersNotu >= 50)
+        {
+            Console.WriteLine(dersAdi + " dersinden geçtiniz");
+            Console.WriteLine(dersAdi + " notunuz : " + dersNotu);
+        }
+        else
+        {
+            Console.WriteLine(dersAdi + " dersinden kaldınız");
+            Console.WriteLine(dersAdi + "notunuz : " + dersNotu);
+        }
+    }
+
+
+}
+
---
+++ Hafta11_NTP_Tekrar_2/Hafta11_NTP_Tekrar_2/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hafta11_NTP_Tekrar_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("--------Sporcu Bilgileri----------");
+            Sporcu futbolcu = new Sporcu();
+            futbolcu.sporcuDal = "Futbol";
+            futbolcu.sporcuAdi = "Maouro Icardi";
+            futbolcu.sporcuYas = 30;
+            futbolcu.sporcuBoy = 1.85;
+            futbolcu.sporcuKilo = 80;
+            futbolcu.sporcuCinsiyet = 'E';
+            futbolcu.bilgiler();
+            Console.WriteLine("--------Antrenman Bilgileri----------");
+            futbolcu.antrenman();
+            Console.WriteLine("****************************");
+            Sporcu voleybolcu = new Sporcu();
+            Console.WriteLine("--------Sporcu Bilgileri----------");
+            voleybolcu.sporcuDal = "Voleybol";
+            voleybolcu.sporcuAdi = "Vargas";
+            voleybolcu.sporcuYas = 20;
+            voleybolcu.sporcuBoy = 1.95;
+            voleybolcu.sporcuKilo = 50;
+            voleybolcu.sporcuCinsiyet = 'K';
+            voleybolcu.bilgiler();
+            Console.WriteLine("--------Antrenman Bilgileri----------");
+            voleybolcu.antrenman();
+            Console.ReadKey();
+
+        }
+    }
+
+    class Sporcu
+    {
+        public String sporcuDal;
+        public String sporcuAdi;
+        public int sporcuYas;
+        public double sporcuBoy;
+        public byte sporcuKilo;
+        public char sporcuCinsiyet;
+
+        public void antrenman()
+        {
+            Console.WriteLine("Günlük 5 km koşu");
+            Console.WriteLine("Günlük 1 saat kardiyo");
+
+            switch(sporcuDal)
+            {
+                case "Futbol":
+                    Console.WriteLine("1 saat şut çalışması yap");
+                    if (sporcuKilo >70)
+                    {
+                        Console.WriteLine("1 saat fazla koşu");
+                    }
+                    break;
+                case "Voleybol":
+                    Console.WriteLine("1 saat zıpla");
+                    break;
+                case "Halter":
+                    Console.WriteLine("1 saat dumbell çalış");
+                    break;
+                default:
+                    Console.WriteLine("İlgili ek çalışma bulunamadı");
+                    break;
+
+            }
+
+        }
+
+        public void bilgiler()
+        {
+            Console.WriteLine("Sporcunun Dalı :" + sporcuDal);
+            Console.WriteLine("Sporcunun Adı :" + sporcuAdi);
+            Console.WriteLine("Sporcunun Yaşı :" + sporcuYas);
+            Console.WriteLine("Sporcunun Boyu :" + sporcuBoy);
+            Console.WriteLine("Sporcunun Kilosu :" + sporcuKilo);
+            if (sporcuCinsiyet == 'E')
+            {
+                Console.WriteLine("Sporcunun Cinsiyeti Erkek");
+
+            }
+            else
+            {
+                Console.WriteLine("Sporcunun Cinsiyeti Kadın");
+            }
+        }
+
+
+    }
+}
+
+
