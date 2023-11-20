7d9c2f1750fca66da8587f4fb9dca3151db276b5
ayhanakkaya <ayhanakkayameb@gmail.com>
2023-11-20 14:59:38 +0300

    metotlar işlendi

---
+++ Hafta11_NTP_Metotlar/Hafta11_NTP_Metotlar/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hafta11_NTP_Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Sayilar mat = new Sayilar();
+            int cikarma=mat.temelMatematik(15, 6);
+            Console.WriteLine("İşlem Sonucu: " + cikarma);
+            int toplama = mat.temelMatematik(5, 6);
+            Console.WriteLine("İşlem Sonucu: " + toplama);
+            int carpma = mat.temelMatematik(5, 5);
+            Console.WriteLine("İşlem Sonucu: " + carpma);
+
+            Console.ReadKey();
+        }
+
+        
+    }
+
+    class Sayilar
+    {
+        public int temelMatematik(int x, int y)
+        {
+            if (x < y)
+            {
+                return x + y;
+            }
+            else if(x>y)
+            {
+                return x - y;
+            }
+
+            else
+            {
+                return x * y;
+            }
+
+
+        }
+    }
+
+    
+}
---
+++ Hafta11_NTP_Metotlar_3/Hafta11_NTP_Metotlar_3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hafta11_NTP_Metotlar_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Geometri dikdortgen = new Geometri();
+            Console.WriteLine("Kısa kenarı gir");
+            int k1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Uzun kenarı gir");
+            int k2 = Convert.ToInt32(Console.ReadLine());
+            int alan=dikdortgen.alanHesapla(k1, k2);
+            Console.WriteLine("Alan=" + alan);
+            dikdortgen.cevreHesapla(k1, k2);
+            Console.ReadKey();
+        }
+    }
+
+    class Geometri
+    {
+        public String sekilAdi;
+        public int kenarSayisi;
+
+        public int alanHesapla(int kisaKenar,int uzunKenar)
+        {
+
+            return kisaKenar * uzunKenar;
+        }
+
+        public void cevreHesapla(int kisaKenar,int uzunKenar)
+        {
+            Console.WriteLine("Çevre=" + 2 * (kisaKenar + uzunKenar));
+        }
+
+
+    }
+}
